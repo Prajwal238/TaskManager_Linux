768b28f2604f2465fff1c5369f6c3bc4d6a1a0c6
Prajwal238 <54891323+Prajwal238@users.noreply.github.com>
2022-04-19 20:40:02 +0530

    Create taskManager.c

---
+++ taskManager.c
@@ -0,0 +1,26 @@
+#include <linux/sched/signal.h>
+#include <linux/init.h> 
+#include <linux/kernel.h> 
+#include <linux/module.h> 
+int startup(void) { 
+  struct task_struct *task; 
+  printk(KERN_INFO “Loading taskManager module \n”); 
+  printk(KERN_INFO “-------------TASK MANAGER-------------”); 
+  for(task=current;task!=&init_task;task=task->parent) 
+  { 
+    printk(KERN_INFO “Process: %-20s State: %ld\t Process ID: %d\n”, task->comm, 
+    task->state, task->pid); 
+  } 
+printk(KERN_INFO “Task Manager closed \n”); 
+return 0; 
+} 
+
+void cleanup(void) { 
+  printk(KERN_INFO “Cleaning up Task Manager”); 
+}
+
+module_init(startup); 
+module_exit(cleanup);
+
+MODULE_LICENSE(“GPL”); 
+MODULE_DESCRIPTION(“Display tasks running in Linux”);
